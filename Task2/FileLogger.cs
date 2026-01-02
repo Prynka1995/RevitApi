fa651ca5ac1235d4f1ee4867d7ef8e2c2067c873
Станислав <stanislav9234@gmail.com>
2026-01-02 03:06:55 +0300

    fix

---
+++ Task2/FileLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    //        Вынесите логирование в отдельный сервис
+    public class FileLogger:ILogger
+    {
+        public const string FileName = "log.txt";
+        //string timestamp = DateTime.Now.ToString();
+        public void Log(string message)
+        { 
+        File.AppendAllText(FileName, message);
+        }
+    }
+}
