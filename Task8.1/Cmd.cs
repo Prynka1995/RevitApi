7803a5d0bb43fa11d483a9b4966dcef2891b8e07
Станислав <stanislav9234@gmail.com>
2026-01-14 00:47:04 +0300

    fix8.1

---
+++ Task8.1/Cmd.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.UI;
+using System.Linq;
+using Task8.Views;
+using Task8.ViewModels;
+using Task8.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Task8.Abstractions;
+
+namespace Task8
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Cmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message,
+                      ElementSet elements)
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddSingleton<ExternalCommandData>(commandData);
+            services.AddSingleton<iSelectionnService, SelectionService>();
+            services.AddSingleton<IGeometryService, GeometryService>();
+            services.AddSingleton<MainWindowViewModel, MainWindowViewModel>();
+            services.AddSingleton<MainWindow, MainWindow>();
+            var provider = services.BuildServiceProvider();
+
+            var mainWindow = provider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Task8.1/Services/WallFilter.cs
@@ -0,0 +1,11 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+
+namespace Task8.Services
+{
+    class WallFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem) => elem is Wall;
+        public bool AllowReference(Reference reference, XYZ position) =>  false;
+    }
+}
